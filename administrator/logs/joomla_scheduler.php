01a263de5a7c7dbd66f006ebf94daffbde7544c5
Kent Hansen <HansenKentc@gmail.com>
2024-12-07 10:32:44 +0000

    Commiting stuck things...

--- administrator/logs/joomla_scheduler.php
+++ administrator/logs/joomla_scheduler.php
@@ -12,3 +12,8 @@
 2024-12-03	13:11:21	INFO	Task> Could not instantiate mail function.
 2024-12-03	13:11:21	INFO	Task> UpdateNotification end
 2024-12-03	13:11:21	INFO	Successfully finished task#03 in 0.60 (net 0.60) seconds.
+2024-12-04	18:19:36	INFO	Running task#02 'Session GC'.
+2024-12-04	18:19:36	INFO	Task> SessionGC end
+2024-12-04	18:19:36	INFO	Successfully finished task#02 in 0.00 (net 0.00) seconds.
+2024-12-04	18:19:55	INFO	Running task#03 'Update Notification'.
+2024-12-04	18:19:56	INFO	Successfully finished task#03 in 0.69 (net 0.69) seconds.
